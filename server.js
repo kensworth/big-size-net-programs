a3322838f1da40edb36a86098c3ce024360a8779
EC2 Default User <ec2-user@ip-172-31-24-179.ec2.internal>
2016-12-17 22:19:17 +0000

    fixed delay xD

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ io.on('connection', (socket) => {
   socket.on('submission', (data) => {
     console.log(data.code);
     var params = {
-      DelaySeconds: 10,
       MessageAttributes: {
         "Submission": {
           DataType: "String",
