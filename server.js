3fb1255daf2634c901d6832a962e8d6eff2dd40c
aksperiod <akenzosalazar@gmail.com>
2016-12-18 18:18:48 -0500

    actually adding favicon

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 const sqs = new AWS.SQS();
 
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/app/assets/favicon.ico'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
